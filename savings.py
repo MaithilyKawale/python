30558b08b923df7e205522c13a541e5095960bfc
MaithilyKawale <maithilyworkspace@gmail.com>
2025-05-07 19:36:23 +0530

    code added

---
+++ savings.py
@@ -0,0 +1,9 @@
+sav=int(input("Enter savings:"))
+if (sav>=5000):
+    print("Go on a road trip with neha.")
+elif (sav>=10000):
+        print("Go on a road trip with neha.")
+elif (sav>=2000):
+        print("Go out with rashmi.")
+else:
+    print("Go on shopping with neha.")
