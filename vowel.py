1633e9aa4efda64c2f0d2ac18d0b6873bc812c08
MaithilyKawale <maithilyworkspace@gmail.com>
2025-05-07 19:29:02 +0530

    code added

---
+++ vowel.py
@@ -0,0 +1,9 @@
+#write a program to check a character is vowel or not
+
+alpha=input("Enter any alphabhet:")
+print("alphabet is",alpha)
+
+if alpha=="a" or alpha=="e" or alpha=="i" or alpha=="o" or alpha=="u" or alpha=="A" or alpha=="E" or alpha=="I" or alpha=="O" or alpha=="U":
+    print(alpha,"is vovel")
+else:
+    print(alpha, "is not vovel")
