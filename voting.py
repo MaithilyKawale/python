48254bd43129cfef2c46bd2a354a31c4b2d89a46
MaithilyKawale <maithilyworkspace@gmail.com>
2025-05-10 20:22:53 +0530

    code added

---
+++ voting.py
@@ -0,0 +1,9 @@
+#write a program to check whether a person is eligible for voting or not
+
+age=int(input("Enter your age:"))
+print("your age is",age)
+
+if age>=18:
+    print("you are eligible for voting")
+else:
+    print("you are not eligible for voting")
