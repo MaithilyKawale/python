9be8bf4122003e83d307f198d9d93b5418a86a8a
MaithilyKawale <maithilyworkspace@gmail.com>
2025-05-15 21:24:28 +0530

    code added

---
+++ largest of three.py
@@ -0,0 +1,10 @@
+a=int(input("enter num1:"))
+b=int(input("enter num2:"))
+c=int(input("enter num3:"))
+
+if (a>b and a>c):
+    print("num1 is greater")
+elif (b>a and b>c):
+    print("num2 is greater")
+else:
+    print("num3 is greater")
