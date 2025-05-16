e89e2f010fa0d1ac514cb2d20beb5072bd051e6d
MaithilyKawale <maithilyworkspace@gmail.com>
2025-05-16 23:25:46 +0530

    code added

---
+++ senior citizen.py
@@ -0,0 +1,10 @@
+#write a python program to check whether a person is senior citizen or not
+
+age=int(input("Enter your age:"))
+print("your age is",age)
+
+
+if age>=50:
+    print("you are senior citizen")
+else:
+    print("you are not senior citizen")
