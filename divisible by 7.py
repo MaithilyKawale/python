264036ec20ae37e37a6c4c5e4f5cdc70267774cb
MaithilyKawale <maithilyworkspace@gmail.com>
2025-05-17 10:28:59 +0530

    code added

---
+++ divisible by 7.py
@@ -0,0 +1,8 @@
+#write a program to check whether a num is divisible by 7 or not
+
+num=int(input("Enter any num:"))
+print("Num is",num)
+if num%7==0:
+    print(num,"is divisible by 7")
+else:
+    print(num,"is not divisible by 7 ")
