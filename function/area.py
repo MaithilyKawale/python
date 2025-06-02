26330c9beec567032f8172a5be26320d852f39ff
MaithilyKawale <maithilyworkspace@gmail.com>
2025-06-02 23:39:55 +0530

    code added

---
+++ function/area.py
@@ -0,0 +1,10 @@
+def area (l,b)->float:
+    return l*b
+
+def main():
+    l=float(input("enter length:"))
+    b= float(input("enter breadth:"))
+    a=area (l,b)
+    print("area is",a)
+
+main()
